1831e7329e28e0ef1bf5f30d7bfa572f5e172b0c
miniTsl <1668685245@qq.com>
2023-07-28 16:40:05 +0800

    增加手机测试代码

--- mobile_cpu_gpu/latency_ort/torch2onnx.py
+++ mobile_cpu_gpu/latency_ort/torch2onnx.py
@@ -6,7 +6,7 @@ config_file = "onnx-models/configs.txt"
 
 with open(config_file, "w") as f:
     for count, config in enumerate(configs):
-        generate_onnx_model(config, count)
+        generate_onnx_model(config, count + 1)
         for config_data in config:
             f.write(str(config_data) + " ")
         f.write("\n")
